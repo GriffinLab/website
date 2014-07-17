94bb4f8b3f348dab265289ebfb3d2f1fe61c9b46
Thomas Rogers <thomas.rogers.12@ucl.ac.uk>
2014-07-17 18:16:33 +0100

    Text tweaks to personal page

--- people_thomas.php
+++ people_thomas.php
@@ -1,12 +1,12 @@
-<?php include 'header.php';?> 
+﻿<?php include 'header.php';?> 
 
 <div class="columns" id="about">
   <h5>Thomas Rogers</h5>
   <p>
     <b>Biography</b><br />
-    I am a postgraduate student in the EPSRC funded 4-year Security Science Doctoral Training Centre. I was originally a physicist but have converted to computer science, although I do like to dabble in physics from time to time.<br />
-    I have previously worked on radar image processing at the Defence Science and Technology Laboratory, and on analysis of data from the LHCb experiement (CERN) at Technische Universität Dortmund.<br />
-Within the Griffin lab, I am currently working on a project funded by Rapiscan Systems Ltd. on the automated analysis of transmission X-ray cargo images.<br />
+    I am a postgraduate student in the EPSRC funded 4-year Security Science Doctoral Training Centre. I was originally a physicist but have converted to computer science, although I do like to dabble in physics from time to time.
+    I have previously worked on radar image processing at the Defence Science and Technology Laboratory, and on analysis of data from the LHCb experiement (CERN) at Technische Universität Dortmund. 
+    <br />Within the Griffin lab, I am currently working on a project funded by Rapiscan Systems Ltd. on the automated analysis of transmission X-ray cargo images.
     My main interest is in applying physics and mathematics to real life security problems.<br /><br />
 
     <b>Education</b><br />
@@ -20,13 +20,10 @@ Within the Griffin lab, I am currently working on a project funded by Rapiscan S
     Special Prize in Computational Physics, Imperial College London (2011)<br /><br />
 
     <b>Publications</b><br />
-    <!-- T. W. Rogers, J. Ollier, E. J. Morton and L. D. Griffin. Beam tracking and the correction of Transmission X-ray Cargo Images. IEEE IST 2014. Pending -->
+    <!-- T. W. Rogers, J. Ollier, E. J. Morton and L. D. Griffin. Reduction of Wobble Artefacts in Images From Mobile Transmission X-ray Vehicle Scanners. IEEE IST 2014. Pending -->
     N. Jaccard, T. Rogers, L. D. Griffin. Automated detection of cars in transmission X-ray images of freight containers. AVSS 2014. Accepted.<br />
     N. S. Blunt, T. W. Rogers, J. S. Spencer, W. M. C. Foulkes. Density matrix quantum Monte Carlo. Phys. Rev. A (2014). Accepted.<br /><br />
 
-    <b>Talks</b><br />
-    N. S. Blunt, T. W. Rogers, J. S. Spencer and W. M. C. Foulkes. Density Matrix Monte Carlo. Quantum Monte Carlo in the Apuan Alps VII (2012) (Did not present)<br />
-    T. Brambach et al. Measurement of the Inclusive Phi Production Cross Section in pp Collisions at √s = 7 TeV. Talk at the LHCb collaboration. (2010) (Did not present)<br />
   </p>
 </div>
 <div class="six columns right" id="group-image">
