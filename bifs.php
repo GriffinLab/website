7985dff3da481614ae6c88d9d0b0469c4b3456de
Theo Boyd <theoboyd@gmail.com>
2014-07-17 17:19:28 +0100

    Expanded (o)BIFs page.

--- bifs.php
+++ bifs.php
@@ -3,8 +3,11 @@
 <div class="six columns" id="about">
   <h5>BIFs</h5>
   <p>Basic Image Features are a significant contribution by this group.</p>
+  <h5>Implementation</h5>
+  <p>Please visit the <a href="https://github.com/GriffinLab/obifs">GitHub repository</a> for implementations and examples of BIFs, including the oriented variant (oBIFs) and histogram variant.</p>
 </div>
 <div class="six columns right" id="group-image">
+  <img src="images/obif_handwriting.png" />
 </div>
 
 <?php include 'footer.php';?>
