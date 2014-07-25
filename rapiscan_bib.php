3aa4f624c76280fc245421516fb757e5ff64654b
Thomas Rogers <thomas.rogers.12@ucl.ac.uk>
2014-07-25 13:33:11 +0100

    Update rapiscan_bib.php
    Added accepted IEEE IST conference paper

--- rapiscan_bib.php
+++ rapiscan_bib.php
@@ -3,6 +3,7 @@
 <div class="columns" id="about">
   <h5>Griffin Lab Publications</h5>
   <p>N. Jaccard, T.W. Rogers, L.D. Griffin, <em>Automated detection of cars in transmission X-ray images of freight containers</em>, 11th IEEE International Conference on Advanced Video and Signal-based Surveillance, 2nd Workshop on Vehicle Retrieval in Surveillance, Seoul, South Korea, 2014 (Accepted)</p>
+  <p>T. W. Rogers, J. Ollier, E. J. Morton, and L.D. Griffin, <em>Reduction of Wobble Artefacts in Images From Mobile Transmission X-ray Vehicle Scanners</em>, 2014 IEEE International Conference on Imaging Systems and Techniques (Accepted)</p>
   <h5>Cargo Image Publications</h5>
   <p>Coming soon.</p>
   <h5>Other Links</h5>
