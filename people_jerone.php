42cb39fbc1bd80f031967a072317ff0488ccbf7d
jeroneandrews <jerone.andrews.13@ucl.ac.uk>
2014-08-20 13:07:11 +0100

    Create people_jerone.php

---
+++ people_jerone.php
@@ -0,0 +1,12 @@
+<?php include 'header.php';?> 
+
+<div class="columns" id="about">
+  <h5>Jerone Andrews</h5>
+  <p>
+   
+  </p>
+</div>
+<div class="six columns right" id="group-image">
+</div>
+
+<?php include 'footer.php';?>
