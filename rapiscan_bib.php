da92e8524320573b6af7c16d94d386ce1f2389ab
Theo Boyd <theoboyd@gmail.com>
2014-08-21 20:43:54 +0100

    Updated publications and links

--- rapiscan_bib.php
+++ rapiscan_bib.php
@@ -1,13 +1,15 @@
 <?php include 'header.php';?> 
 
 <div class="columns" id="about">
-  <h5>Griffin Lab Publications</h5>
+  <h5>Project Publications</h5>
   <p>N. Jaccard, T.W. Rogers, L.D. Griffin, <em>Automated detection of cars in transmission X-ray images of freight containers</em>, 11th IEEE International Conference on Advanced Video and Signal-based Surveillance, 2nd Workshop on Vehicle Retrieval in Surveillance, Seoul, South Korea, 2014 (Accepted)</p>
   <p>T. W. Rogers, J. Ollier, E. J. Morton, and L.D. Griffin, <em>Reduction of Wobble Artefacts in Images From Mobile Transmission X-ray Vehicle Scanners</em>, 2014 IEEE International Conference on Imaging Systems and Techniques (Accepted)</p>
-  <h5>Cargo Image Publications</h5>
-  <p>Coming soon.</p>
+  <p><a href="https://sites.google.com/site/lewisdgriffin/publications">All Griffin Lab publications</a></p>
+  <!--h5>Cargo Image Publications</h5>
+  <p>Coming soon.</p-->
   <h5>Other Links</h5>
   <p>S. Nance, <em><a href="http://www.kmimediagroup.com/border-security-emergency-preparedness/422-articles-bcd/cargo-screening">Cargo Screening</a></em>, Border Security &amp; Emergency Preparedness, KMI Media Group, 2014</p>
+  <p>K. Mowery et al., <em><a href="https://radsec.org">Security Analysis of a Full-Body Scanner</a></em>, Proceedings of the 23rd USENIX Security Symposium, 2014</p>
 </div>
 <div class="six columns right" id="group-image">
 </div>
