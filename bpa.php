a8361e67f770803a5e0bcb045bb230326d5766e5
Theo Boyd <theoboyd@gmail.com>
2014-09-22 11:47:24 +0100

    Writeup of Manos' BPA page.

--- bpa.php
+++ bpa.php
@@ -1,8 +1,18 @@
 <?php include 'header.php';?> 
 
-<div class="six columns" id="about">
+<div class="columns" id="about">
   <h5>Biological Pattern Analysis</h5>
   <p>Another major area of research for this group.</p>
+  <br />
+  <p><img src="images/bpa1.png" height="250px"></p>
+  <p>Changes in the <em>drosophila notum</em> between 17.5h (left) and 22h (right) after pupae formation. The microscopy images visualise two aspects of the tissue organisation. The polygonal mesh of cell outlines is visible due to expression of green fluorescent protein (GFP) attached to a cell adhesion molecule (E-Cadherin-GFP), and the array of mostly isolated bristle precursor cells is visible due to GFP expression attached to a signalling protein (Neu:GFP). (Scale bars: 20&micro;m). An increase in the degree of organisation (order) is observed when these two images are compared.</p>
+  <br />
+  <p><img src="images/bpa2.png" height="250px"></p>
+  <p>Schematic of the Delaunay triangles' sides entropy measure. From a point pattern (left) a Delaunay triangulation (middle) is constructed. From the triangulation, side length histograms (right) are determined for shortest (top), intermediate (middle) and longest sides (bottom). Smoothed histograms are created by kernel-density estimation and used for entropy estimation.</p>
+  <br />
+  <p><img src="images/bpa2.png" height="250px"></p>
+  <p>Schematic of the Delaunay triangles entropy measure. At left are two example patterns; moving right, their Delaunay triangulationsl next, the triangle size histogram (smoothed); and finally the triangle shape histogram (raw data and smoothed). The more disordered lower pattern has wider histograms than the more ordered upper pattern.</p>
+  <p></p>
 </div>
 <div class="six columns right" id="group-image">
 </div>
