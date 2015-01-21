878f5532617bbf9cf2e18b7ce6abeb5d56cede08
Jerone Andrews <jerone.andrews.13@ucl.ac.uk>
2015-01-21 16:39:11 +0000

    Update people_jerone.php

--- people_jerone.php
+++ people_jerone.php
@@ -1,9 +1,35 @@
 <?php include 'header.php';?> 
 
 <div class="columns" id="about">
-  <h5>Jerone Andrews</h5>
+  <h5>Jerone Theodore Alexander Andrews</h5>
   <p>
-   
+    <b>Biography</b><br />
+    My undergraduate MSci degree from King's College London was in Mathematics, and in my final year I took an interest in the applied side, particularly disordered systems. Though, my interests now lie in computational statistics and machine learning applied to anomaly detection, within the application domain of security images.
+<br>
+I hold a funded 4-year studentship (MRes and PhD) awarded by the Engineering and Physical Sciences Research Council (EPSRC), and Rapiscan Systems Ltd. who are a global provider of security scanners (person, hand luggage, luggage, cargo and vehicle) and related products. My MRes and PhD are administered with the Security Science Doctoral Research Training Centre (SECReT) within University College London’s Jill Dando Institute of Security and Crime Science. 
+<br>
+I am also a member of University College London’s Centre for Computational Statistics & Machine Learning Centre, and I am supervised by both the Computer Science (Dr. Lewis Griffin, Vision and Imaging Science Research Group) and Statistical Science (Dr. James Nelson) department.
+<br /><br />
+
+    <b>Education</b><br />
+     Doctor of Philosophy (PhD), <em>Computational Statistics & Machine Learning, and Security Science, </em> University College London (2014–2017).<br />
+<ol><em>Research focus:</em> Automated detection of suspicious cargo based on X-ray transmission images. <br>My research is part of a larger project concerned with automating the analysis of images from a novel type of scanner which images rail cargo as it passes through an arch without slowing. Other team members are working on comparing cargo images to electronic manifest data, and detecting specific items (e.g. cigarettes, stowaways, cars, etc.). My research aims to provide a final line of defense – to pick up oddities that we have never seen before but which a human operator would spot.
+</ol>
+    Master of Research (MRes), <em>Security Science, </em>University College London (2013–2014).<br />
+    Master of Science (MSci), <em>Mathematics, </em>King's College London (2008–2013).<br /><br />
+
+    <b>Teaching</b><br />
+    Course Code: STAT6101, Introductory Statistical Methods and Computing (Term 1 & 2).<br />
+    Course Code: COMP2003, Mathematics and Statistics (Term 1).<br />
+    Course Code: COMP3096, Research Group Project (Term 1 & 2).<br />
+    Laboratory Sessions: BSc/MSci Computer Science, MSc Financial Computing, and MSc Computer Science Introduction to Computer Science Computing Facilities (Term 1).<br /><br />
+
+    <b>Research interests</b><br />
+	<li>Anomaly, novelty, and outlier detection;</li>
+<li>Machine learning, concept learning, and non-probabilistic representational learning;</li>
+<li>Computer vision, and image processing;</li>
+<li>Unary classification methods.</li><br /><br />
+
   </p>
 </div>
 <div class="six columns right" id="group-image">
