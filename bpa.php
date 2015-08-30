7ef53b934e88de49b08238647a15be17ca51d1fe
Theo Boyd <theoboyd@gmail.com>
2015-08-30 22:27:46 +0100

    Fixed spelling

--- bpa.php
+++ bpa.php
@@ -14,7 +14,7 @@
   <p>Schematic of the Delaunay triangles entropy measure. At left are two example patterns; moving right, their Delaunay triangulationsl next, the triangle size histogram (smoothed); and finally the triangle shape histogram (raw data and smoothed). The more disordered lower pattern has wider histograms than the more ordered upper pattern.</p>
   <p></p>
   <h5>References</h5>
-  <p>Bouremom S., Protonotarios E., Griffin L. D. (2015). <i>Quantifying texture scale></i>. ICSIA '15<br />
+  <p>Bouremoum S., Protonotarios E., Griffin L. D. (2015). <i>Quantifying texture scale></i>. ICSIA '15<br />
   Grove J., Metcalf D. J., Knight A. E., Wavre-Shapton S. T., Sun T., Protonotarios E. D., Griffin L. D., Lippincott-Schwartz J. and Marsh M. (2014). <i>Flat clathrin lattices: stable features of the plasma membrane</i>. Molecular Biology of the Cell [<a href="http://www.molbiolcell.org/content/25/22/3581.short">LINK</a>]<br />
   Protonotarios, E. D., Baum, B., Johnston, A., Hunter, G. L., and Griffin, L. D. (2014). <i>An absolute interval scale of order for point patterns</i>. J R Soc Interface 11(99) doi:10.1098/rsif.2014.0342 [<a href="http://rsif.royalsocietypublishing.org/content/11/99/20140342.short">LINK</a>]<br>
   Protonotarios M., Ciurea-Ilcus S. and Griffin L. D. (2012). <i>Objective measures of apparent order of point patterns</i>. In: Proc. Predicting Perceptions 2012 [<a href="http://www.macs.hw.ac.uk/texturelab/files/publications/perceptions/Objective_Measures_of_Apparent_Order_of_Point_Patterns.pdf">LINK</a>]<br>
