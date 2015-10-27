47b13e26ee949d6962c9da72feb5c20a9c8f84f9
Theo Boyd <theoboyd@gmail.com>
2015-10-27 12:08:04 +0000

    Removed broken UCL website urls, added Thomas T

--- people.php
+++ people.php
@@ -6,22 +6,23 @@
   <h5>Research Associate</h5>
   <p><a href="http://www0.cs.ucl.ac.uk/people/N.Jaccard">Dr Nicolas Jaccard</a></p>
   <h5>PhD/EngD Students</h5>
-  <p><a href="http://www0.cs.ucl.ac.uk/people/T.Rogers">Thomas Rogers</a> (<a href="people_thomas.php">website</a>)</p>
-  <p><a href="https://www.ucl.ac.uk/secret/current_students/2013_cohort/2013_cohort_profiles/jerone-andrews">Jerone Andrews</a> (<a href="people_jerone.php">website</a>)</p>
-  <p><a href="http://www0.cs.ucl.ac.uk/people/S.Bouremoum">Sami Bouremoum</a> (<a href="http://bouremoum.com">website</a>)</p>
-  <p><a href="http://www0.cs.ucl.ac.uk/people/T.Boyd">Theodore Boyd</a> (<a href="http://www0.cs.ucl.ac.uk/staff/T.Boyd/">website</a>)</p>
-  <p><a href="http://www0.cs.ucl.ac.uk/people/E.Protonotarios">Manos Protonotarios</a> (<a href="http://www.protonotarios.net/">website</a>)</p>
+  <p><a href="people_jerone.php">Jerone Andrews</a></p>
+  <p><a href="http://bouremoum.com">Sami Bouremoum</a></p>
+  <p><a href="http://www0.cs.ucl.ac.uk/staff/T.Boyd">Theodore Boyd</a></p>
   <p><a href="http://www0.cs.ucl.ac.uk/people/D.Mylonas">Dimitris Mylonas</a> (<a href="http://colournaming.com/">website</a> | <a href="https://scholar.google.co.uk/citations?user=I7W_IuYAAAAJ&hl=en">pubs</a>)</p>
+  <p><a href="http://www.protonotarios.net">Manos Protonotarios</a></p>
+  <p><a href="people_thomas.php">Thomas Rogers</a></p>
+  <p>Thomas Tanay</p>
   <h5>Visitors</h5>
   <p><a href="http://gait.buaa.edu.cn/~zqy">Dr Zhao Qi-Yang</a></p>
   <h5>Completed PhDs</h5>
   <p><a href="http://www0.cs.ucl.ac.uk/people/M.OReilly">Martin O'Reilly</a> (<em>Reconstruction of neural circuits</em>) (<a href="http://www.ucl.ac.uk/~ucbpmor/">website</a>)</p>
-  <p><a href="http://www0.cs.ucl.ac.uk/people/A.Newell">Andrew Newell</a> (<em>Object category recognition</em>) (<a href="http://www.ucl.ac.uk/complex/research/students/alumni/newell">website</a>)</p>
+  <p><a href="http://www0.cs.ucl.ac.uk/people/A.Newell">Andrew Newell</a> (<em>Object category recognition</em>)</p>
   <p>Alex Nasrallah</a> (<em>Multi-local image structure</em>)</p>
   <p>Luiza Larsen</a> (<em>Polarised light imaging of the CNS</em>)</p>
   <h5>Past Research Fellows</h5>
-  <p><a href="http://www0.cs.ucl.ac.uk/people/A.Newell">Andrew Newell</a> (<a href="http://www.ucl.ac.uk/complex/research/students/alumni/newell">website</a>)</p>
-  <p>Martin Lillholm</a> (<a href="http://www0.cs.ucl.ac.uk/staff/M.Lillholm/">website</a>)</p>
+  <p><a href="http://www0.cs.ucl.ac.uk/people/A.Newell">Andrew Newell</a></p>
+  <p><a href="http://www0.cs.ucl.ac.uk/staff/M.Lillholm/">Martin Lillholm</a></p>
   <h5>Group Meetings</h5>
   <p><a href="http://goo.gl/CL6Gga">Weekly lab lunch schedule</a></p>
 </div>
