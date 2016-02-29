aefcacfeed9fd444d220429e476028dee9896da6
Theo Boyd <theoboyd@gmail.com>
2016-02-29 15:44:18 +0000

    News link

--- rapiscan.php
+++ rapiscan.php
@@ -14,8 +14,8 @@
   <p><em><a href="http://www.ima.org.uk/conferences/conferences_calendar/4th_mathematics_in_defence.cfm.html">4th IMA Conference on Mathematics in Defence</a></em>. Satellite Applications Catapult, Harwell Oxford, UK, 26th November 2015</p>
   <p><em><a href="https://www.ucl.ac.uk/jdi/events/crime-science-conf/icsc-current">9th International Crime Science Conference</a></em>. British Library, London, UK, 16th July 2015</p>
   <h5>Other Links</h5>
-  <p>Dearden L. <em><a href="http://www.independent.co.uk/news/world/europe/paris-attacks-almost-800-pump-action-shotguns-seized-by-italian-police-on-way-to-belgium-germany-and-a6752106.html">Paris attacks: Almost 800 pump-action shotguns seized by Italian police on way to Belgium, Germany and Holland
-</a></em>. The Indepenent, 2015</p>
+  <p>Dodd V. <em><a href="http://www.theguardian.com/uk-news/2016/feb/28/online-shopping-boom-criminals-smuggle-guns-uk-police-chief">Online retail boom helping criminals smuggle guns into UK, says police chief</a></em>. The Guardian, 2016</p>
+  <p>Dearden L. <em><a href="http://www.independent.co.uk/news/world/europe/paris-attacks-almost-800-pump-action-shotguns-seized-by-italian-police-on-way-to-belgium-germany-and-a6752106.html">Paris attacks: Almost 800 pump-action shotguns seized by Italian police on way to Belgium, Germany and Holland</a></em>. The Indepenent, 2015</p>
   <p>Bacchi U. <em><a href="http://www.ibtimes.co.uk/greece-seized-tanker-haddad-1-concealed-5000-shotguns-libya-islamists-1518372">Greece: Seized cargo ship Haddad 1 concealed 5,000 shotguns 'for Libya Islamists'</a></em>. International Business Times, 2015</p>
   <p>Friedersdorf C. <em><a href="http://www.theatlantic.com/politics/archive/2015/10/the-nypd-is-using-mobile-x-rays-to-spy-on-unknown-targets/411181/?single_page=true">The NYPD Is Using Mobile X-Ray Vans to Spy on Unknown Targets</a></em>. The Atlantic, 2015</p>
   <p>Nance S. <em><a href="http://www.kmimediagroup.com/border-security-emergency-preparedness/422-articles-bcd/cargo-screening">Cargo Screening</a></em>. Border Security &amp; Emergency Preparedness, KMI Media Group, 2014</p>
