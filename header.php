1ef3de50b5f6fb5279447a30fb714d6c41e42ac8
Nicolas Jaccard <nicolas.jaccard@gmail.com>
2016-06-02 10:11:56 +0100

    More analytics and twitter integration

--- header.php
+++ header.php
@@ -16,6 +16,17 @@
   <link rel="stylesheet" href="stylesheets/overrides.css">
 
   <script src="javascripts/modernizr.foundation.js"></script>
+    
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-78680552-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </head>
 <body>
 
--- index.php
+++ index.php
@@ -1,15 +1,5 @@
 <?php include 'header.php';?> 
 
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-78680552-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 
 <div class="six columns" id="about">
   <h5>Research</h5>
@@ -21,7 +11,7 @@
   </div>
 </div>
 <div class="six columns right" id="group-image">
-  <a class="twitter-timeline" href="https://twitter.com/search?q=from%3Anicjac%20OR%20from%3A%40TWRogersIII" data-widget-id="733680235024162820">Tweets about from:nicjac OR from:@TWRogersIII</a>
+  <a class="twitter-timeline" href="https://twitter.com/search?q=from%3Anicjac%20OR%20from%3A%40TWRogersIII" data-widget-id="733680235024162820">Tweets from Lewis Lab members</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 </div>
 
